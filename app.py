fe890310ea60efccb38195ecac287bf9e28b28b6
Shubhi Jain <shubhijain@caltech.edu>
2016-03-05 03:15:23 -0800

    Basic working?

--- app.py
+++ app.py
@@ -3,8 +3,13 @@
 #
 
 import os
+import string
+import random
 
 from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
 from flask.ext.sqlalchemy import SQLAlchemy
 
 ################################## CONFIG ######################################
@@ -14,14 +19,36 @@ db = SQLAlchemy(app)
 
 from models import URL
 from forms import URLShortenerForm
+
+################################# LIBRARY ######################################
+# Generate validation string
+def id_generator(size=8, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 ################################## ROUTES ######################################
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Hi'
+    form = URLShortenerForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        existing_url = URL.query.filter_by(url=url).first()
+        if existing_url:
+            return render_template('index.html', form=form, url_shortened=existing_url.url_shortened)
+        else:
+            new_url = URL(url=url, url_shortened=id_generator())
+            db.session.add(new_url)
+            db.session.commit()
+            return render_template('index.html', form=form, url_shortened=new_url.url_shortened)
+    return render_template('index.html', form=form)
 
 @app.route('/<url>')
 def url(url):
-    return 'hi'
+    existing_url = URL.query.filter_by(url_shortened=url).first()
+    if existing_url:
+        return redirect(existing_url.url)
+    else:
+        return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -5,7 +5,7 @@ class URL(db.Model):
     __tablename__ = 'urls'
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
+    url = db.Column(db.String(), unique=True)
     url_shortened = db.Column(db.String())
 
     def __repr__(self):
