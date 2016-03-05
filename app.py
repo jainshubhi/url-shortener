72fe9b9c4fe7209f7cab157f6ae9322fb8c8d1a5
Shubhi Jain <shubhijain@caltech.edu>
2016-03-05 04:29:03 -0800

    adding comments

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ from forms import URLShortenerForm
 def id_generator(size=8, chars=string.ascii_uppercase + string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+# take only important part of url
 def remove_slashes(url):
     return url.split('//')[1]
 
@@ -36,13 +37,16 @@ def index():
         url = remove_slashes(form.url.data)
         existing_url = URL.query.filter_by(url=url).first()
         if existing_url:
+            # url is already in db so serve that shortened url
             return render_template('index.html', form=form,
                 url_shortened=existing_url.url_shortened,
                 site_address=os.environ['SITE_ADDRESS'])
         else:
+            # make sure shortened_url doesn't already exist
             le_id = id_generator()
             while URL.query.filter_by(url_shortened=le_id).first():
                 le_id = id_generator()
+            # add url to db
             new_url = URL(url=url, url_shortened=le_id)
             db.session.add(new_url)
             db.session.commit()
@@ -54,8 +58,10 @@ def index():
 @app.route('/<url>')
 def url(url):
     existing_url = URL.query.filter_by(url_shortened=url).first()
+    # if shortened url exists
     if existing_url:
         return redirect('https://' + existing_url.url)
+    # redirect to mainpage if this is a random url
     else:
         return redirect(url_for('index'))
 
